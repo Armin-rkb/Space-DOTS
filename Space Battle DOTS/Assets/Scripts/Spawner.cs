6ca723d2345ef47a65453b53193cf3bc933dc6a3
Armin Karimi Birgani <armin.rkb@gmail.com>
2020-05-13 14:55:44 +0200

    [Add] ECS spawning in grid.

--- Space Battle DOTS/Assets/Scripts/Spawner.cs
+++ Space Battle DOTS/Assets/Scripts/Spawner.cs
@@ -11,10 +11,16 @@ public class Spawner : MonoBehaviour
 
     // Normal GameObject
     [SerializeField] private GameObject gameObjectPrefab = null;
-    
+
+    // Grid Spawn settings
+    [SerializeField] private int rows = 10;
+    [SerializeField] private int collums = 10;
+    [Range(0.1f, 2.5f),SerializeField] private float offset = 1f;
+
     // Converted Object
     private Entity entityPrefab;
 
+    // Manager and world
     private EntityManager entityManager;
     private World defaultWorld;
 
@@ -27,9 +33,19 @@ public class Spawner : MonoBehaviour
         GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(defaultWorld, null);
         entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(gameObjectPrefab, settings);
 
-        InstantiateEntity(new float3(2f, -5f, -6f));
+        InstantiateEntityGrid(rows, collums, offset);
     }
 
+    private void InstantiateEntityGrid(int a_rows, int a_collums, float a_offset)
+    {
+        for (int x = 0; x < a_rows; x++)
+        {
+            for (int y = 0; y < a_collums; y++)
+            {
+                InstantiateEntity(new float3(x * a_offset, y * a_offset, 6f));
+            }
+        }
+    }
 
     private void InstantiateEntity(float3 position)
     {
