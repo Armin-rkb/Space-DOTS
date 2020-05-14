53323724b7e7f901428cf41351df54a535bb2aca
Armin Karimi Birgani <armin.rkb@gmail.com>
2020-05-14 09:37:07 +0200

    [Add] Created sin wave with ecs objects.

---
+++ Space Battle DOTS/Assets/Scripts/Data/MoveSpeedData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+
+/// <summary>
+/// Data always needs to be a struct and derive from IComponentData.
+/// Because we don't use MonoBehavior we need to add "GenerateAuthoringComponent" to- 
+/// this script for Unity to attach it do a game object.
+/// </summary>
+
+[GenerateAuthoringComponent]
+public struct MoveSpeedData : IComponentData
+{
+    // No functions, Components hold data only so there is no logic involved.
+    public float Value;
+}
---
+++ Space Battle DOTS/Assets/Scripts/Systems/SinWaveSystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+// Derives from Component system, this will only use the main thread.
+public class SinWaveSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation a_trans, ref MoveSpeedData a_moveSpeedData, ref SinWaveData a_waveData) =>
+        {
+            float zPos = a_waveData.amplitude * math.sin((float)Time.ElapsedTime * a_moveSpeedData.Value
+                + a_trans.Value.x * a_waveData.xOffset + a_trans.Value.y * a_waveData.yOffset);
+            a_trans.Value = new float3(a_trans.Value.x, a_trans.Value.y, zPos);
+        });
+    }
+}
