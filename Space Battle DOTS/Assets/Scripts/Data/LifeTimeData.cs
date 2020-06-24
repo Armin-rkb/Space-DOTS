7109b93c90b9d751ef391ed6e24e8b7ad40facde
Armin Karimi Birgani <armin.rkb@gmail.com>
2020-06-24 17:17:35 +0200

    [Add] LifeTimeSystem - Entities with LifeTimeData will be removed after a certain amount of time given. This is applied to the lasers. [Fix] Lasers - Lasers are now correctly being shot with the firerate.

---
+++ Space Battle DOTS/Assets/Scripts/Data/LifeTimeData.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct LifeTimeData : IComponentData
+{
+    public float secondsToDestroy;
+    public float timeAlive;
+}
---
+++ Space Battle DOTS/Assets/Scripts/Systems/LifeTimeSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Physics.Extensions;
+using Unity.Physics.Systems;
+using Unity.Transforms;
+using UnityEngine;
+using static Unity.Mathematics.math;
+
+public class LifeTimeSystem : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        EntityCommandBuffer entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+        float deltaTime = Time.DeltaTime;
+
+        Entities.ForEach((Entity a_entity, ref LifeTimeData a_lifeTimeData) =>
+        {
+            a_lifeTimeData.timeAlive += deltaTime;
+
+            if (a_lifeTimeData.timeAlive >= a_lifeTimeData.secondsToDestroy)
+            {
+                entityCommandBuffer.DestroyEntity(a_entity);
+            }
+        }).Run();
+
+        commandBufferSystem.AddJobHandleForProducer(this.Dependency);
+    }
+}
